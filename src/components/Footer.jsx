7b3245b48bd7d502c67b613afbc848e5bb1ec1f4
as548 <113757091+as548@users.noreply.github.com>
2023-12-21 13:21:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
     <Box bgColor={'blackAlpha.900'} color={'blackAlpha.700'} minH={'48'} px={'16'}py={['16','8']}>
         <Stack direction={['column','row']} h={'full'} alignItems={'center'}>
             <VStack w={'full'} alignItems={['center','flex-start']}>
-               <Text fontWeight={'bold'}color={'blackAlpha.900'}>About Us</Text>
-               <Text fontSize={'sm'} letterSpacing={'widest'} textAlign={['center','left']} color={'blackAlpha.900'}>
+               <Text fontWeight={'bold'}color={'whiteAlpha.900'}>About Us</Text>
+               <Text fontSize={'sm'} letterSpacing={'widest'} textAlign={['center','left']} color={'whiteAlpha.900'}>
                 We are the best crypto trading app in India, we provide guidance at a very cheap price.</Text>
 
             </VStack>
             <VStack >
                 <Avatar boxSize={'28'} mt={['4','0']}/>
-                <Text color={'blackAlpha.900'}>Our Founder</Text>
+                <Text color={'whiteAlpha.900'}>Our Founder</Text>
 
             </VStack>
 
